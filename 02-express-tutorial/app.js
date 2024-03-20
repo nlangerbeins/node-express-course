b55bcbcff766d149551298280fbcebe379b4da4c
Natallia Langerbeins <natallia.langerbeins@gmail.com>
2024-03-20 23:40:16 +0100

    middleware and HTTP methods

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,63 +1,16 @@
 const express = require('express');
 const app = express();
-const { products } = require('./data');
 
-app.use(express.static('./public'));
+const peopleRouter = require('./routes/people');
 
-app.get('/api/v1/products', (req, res) => {
-  res.status(200).json(products);
-});
+// static assets
+app.use(express.static('./methods-public'));
 
-app.get('/api/v1/products/:productID', (req, res) => {
-  const { productID } = req.params;
-  const singleProduct = products.find((product) => {
-    return product.id === Number(productID);
-  });
+// Middleware to parse url-encoded and JSON request bodies
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-  if (!singleProduct) {
-    return res.status(404).send('That product was not found');
-  }
-  return res.json(singleProduct);
-});
-
-app.get('/api/v1/query', (req, res) => {
-  console.log(req.query);
-
-  const { search, limit, price } = req.query;
-  let sortedProducts = [...products];
-
-  // Filter by search term
-  //   if (search) {
-  //     sortedProducts = sortedProducts.filter((product) =>
-  //       product.name.startsWith(search)
-  //     );
-  //   }
-
-  // Apply limit
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, parseInt(limit));
-  }
-
-  // Filter by maximum price
-  if (price) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.price < parseFloat(price);
-    });
-  }
-
-  // Filter by regular expression
-  if (search) {
-    const regex = new RegExp(search, 'i');
-    sortedProducts = sortedProducts.filter((product) =>
-      regex.test(product.name)
-    );
-  }
-
-  res.status(200).json(sortedProducts);
-});
-
-app.all('*', (req, res) => {
-  res.status(404).send('<h4>Page not found</h4>');
-});
+// router
+app.use('/api/v1/people', peopleRouter);
 
 app.listen(3000);
