602ee483c19646ab5e8b18b829883165656b5823
Natallia Langerbeins <natallia.langerbeins@gmail.com>
2024-04-09 00:52:55 +0200

    CRUD operations and error handling

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,12 +4,19 @@ const port = 3000;
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 //middleware
+app.use(express.static('./public'));
 app.use(express.json());
 
 //routes
 app.use('/api/v1/tasks', tasks);
+
+// not found page
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
